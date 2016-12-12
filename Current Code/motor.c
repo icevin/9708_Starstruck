8923e86b6f1fa94f498ab336c1df5ed730c60d57
Kevin Tang <kevintang2023@gmail.com>
2016-12-12 09:57:26 -0500

    Hotfixes during SB 12/10/16
    All changes/hotfixes made during 12/10/2016 South Brunswick VRC Event

--- Current Code/motor.c
+++ Current Code/motor.c
@@ -1,6 +1,10 @@
 #ifndef MOTORC
 #define MOTORC
 
+#define MOTOR_MAX_VALUE 127
+#define MOTOR_MIN_VALUE -127
+#define slew 5
+
 int motorReq[10];
 int motorCur[10];
 
@@ -10,14 +14,34 @@ int forceSetMotor(int port, int power);
 int reqMotor(int port, int power);
 int setSlew(int port, int slew);
 
+task motorSlewControl() {
+	while(true) {
+		for(int x = 0; x<10; x++) {
+			if(motorReq[x] == motorCur[x]) {
+				continue;
+			}
+			
+			if(motorCur[x] < motorReq[x]) {
+				motorCur[x] += slew;
+			} else if(motorCur[x] > motorReq[x]) {
+				motorCur[x] -= slew;
+			}
+			
+			forceSetMotor(x, motorCur[x]);
+			wait1Msec(LOOPSPEED+LOOPSPEED);
+		}
+	}
+}	
+
 int forceSetMotor(int port, int power) {
-	motorCur[port-1] = power;
-	motor[port] = power;
+	motorCur[port] = power;
+	if(abs(power) < MOTOR_MAX_VALUE) {
+		motor[port+1] = power;
+	}
 }
 
 int reqMotor(int port, int power, speed) {
-	motorReq[port-1] = power;
-	
+	motorReq[port] = power;
 }
 
 
